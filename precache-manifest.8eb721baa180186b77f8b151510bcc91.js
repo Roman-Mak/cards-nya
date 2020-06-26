0115b470fedf4c5359b2cda189fa23bbf5c018fe
Roman-Mak <meller1758@gmail.com>
2020-06-26 17:54:42 +0300

    Updates

---
+++ precache-manifest.8eb721baa180186b77f8b151510bcc91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bff1e7b45bdde8bc4076db896bfb3b0",
+    "url": "/cards-nya/index.html"
+  },
+  {
+    "revision": "68282afc501e3bd8d287",
+    "url": "/cards-nya/static/css/main.2cc2d7cf.chunk.css"
+  },
+  {
+    "revision": "633a73df539f2d20f22a",
+    "url": "/cards-nya/static/js/2.e92bcc4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards-nya/static/js/2.e92bcc4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68282afc501e3bd8d287",
+    "url": "/cards-nya/static/js/main.f7661439.chunk.js"
+  },
+  {
+    "revision": "c358d079e0300b0ec379",
+    "url": "/cards-nya/static/js/runtime-main.52a95591.js"
+  }
+]);

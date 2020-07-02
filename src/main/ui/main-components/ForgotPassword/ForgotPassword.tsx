06e65639215cd31d6c32428bf16319aa4096b71c
Roman-Mak <meller1758@gmail.com>
2020-07-02 16:57:53 +0300

    forgot Password BLL & DALL

--- src/main/ui/main-components/ForgotPassword/ForgotPassword.tsx
+++ src/main/ui/main-components/ForgotPassword/ForgotPassword.tsx
@@ -1,15 +1,35 @@
-import React, {useState} from "react";
+import React, {useCallback, useState} from "react";
 import styles from "./ForgotPassword.module.css"
 import Input from "../../common/Input/Input";
 import Button from "../../common/Button/Button";
+import {useDispatch, useSelector} from "react-redux";
+import {resetPassword} from "../../../bll/forgot-password-reducer";
+import {AppStateType} from "../../../bll/store";
+import { Redirect } from "react-router-dom";
 
 const ForgotPassword = () => {
-    const [email, setEmail] = useState("");
+    const [email, setEmail] = useState<string>("");
+
+    const dispatch = useDispatch();
+    const error = useSelector((store: AppStateType) => store.forgotPassword.error);
+    const resetSuccess = useSelector((store: AppStateType) => store.forgotPassword.success);
+
+    const resetPass = useCallback(
+        () => {
+            dispatch(resetPassword(email))
+        }, [email, dispatch]
+    );
+
+    if (resetSuccess) return <Redirect to={"/set-new-password"}/>;
 
     return (
         <div className={styles.container}>
-            <Input onChangeFunc={setEmail} value={email} type={"email"} placeholder={"your E-mail"}/>
-            <Button name={"Send"} onClickFunc={() => {}}/>
+            <div>Forgot Password</div>
+            <Input onChangeFunc={setEmail} value={email} type={"email"} placeholder={"your e-mail"}/>
+            {
+                error && <span className={styles.error}>{error}</span>
+            }
+            <Button name={"Send"} onClickFunc={resetPass}/>
         </div>
     );
 };

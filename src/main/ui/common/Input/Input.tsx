6185aaef69d2c5c175fc198fa8af7b3586b80b00
Roman-Mak <meller1758@gmail.com>
2020-07-02 17:06:19 +0300

    small input fix

--- src/main/ui/common/Input/Input.tsx
+++ src/main/ui/common/Input/Input.tsx
@@ -4,7 +4,7 @@ import styles from "./Input.module.css";
 type InputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
     & {onChangeFunc: (value: string) => void };
 
-const Input = (props: InputPropsType) => {
+const Input = React.memo((props: InputPropsType) => {
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
         props.onChangeFunc(e.currentTarget.value);
     };
@@ -14,6 +14,6 @@ const Input = (props: InputPropsType) => {
             <input type={props.type} placeholder={props.placeholder} value={props.value} onChange={onChange}/>
         </div>
     );
-};
+});
 
 export default Input;
